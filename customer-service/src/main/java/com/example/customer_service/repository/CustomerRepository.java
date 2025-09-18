58595940f1e2ced1a1b60072b5a974d64c5e26ab
Pro_badami <sgrahul2507@gmail.com>
2025-09-18 23:58:41 +0530

    backend

---
+++ customer-service/src/main/java/com/example/customer_service/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.customer_service.repository;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.example.customer_service.model.Customer;
+
+public interface CustomerRepository  extends MongoRepository<Customer,String>{
+	Customer findByEmail(String email);
+}
