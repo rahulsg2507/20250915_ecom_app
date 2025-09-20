8f51928fc52bc9bdb513a4bd4c75a93fd6bfd0ee
Pro_badami <sgrahul2507@gmail.com>
2025-09-20 09:12:41 +0530

    Delete customer-service directory

--- customer-service/src/main/java/com/example/customer_service/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.customer_service.repository;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.customer_service.model.Customer;
-
-public interface CustomerRepository  extends MongoRepository<Customer,String>{
-	Customer findByEmail(String email);
-}
